78cbb1ee95c528101163f53088e7fc846884c32e
bdarley@ad.unc.edu <bdarley@ad.unc.edu>
2021-09-27 08:28:21 -0400

    moved properties of IPagedResponse<T> to IPagedResponse parent interface

--- UNC.Services/Interfaces/Response/IPagedResponse.cs
+++ UNC.Services/Interfaces/Response/IPagedResponse.cs
@@ -5,15 +5,14 @@ namespace UNC.Services.Interfaces.Response
 {
     public interface IPagedResponse : IResponse
     {
-
-    }
-    public interface IPagedResponse<T> : IPagedResponse
-    {
         int PageSize { get; set; }
         int Index { get; set; }
         int TotalRecords { get; set; }
         int TotalPages();
-
+    }
+    public interface IPagedResponse<T> : IPagedResponse
+    {
+        
         IEnumerable<T> Entities { get; set; }
     }
 }
